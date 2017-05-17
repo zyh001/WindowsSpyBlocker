991ee60acdca16c3c77abc9848b75df61d35d23d
CrazyMax <crazy-max@users.noreply.github.com>
2017-05-17 22:38:08 +0200

    Update app.conf if keys are missing on update (Issue #53)

--- app/utils/config/config.go
+++ app/utils/config/config.go
@@ -84,14 +84,21 @@ func getAppCfg() (appConf, error) {
 	var cfg appConf
 	cfgPath := path.Join(pathu.Current, "app.conf")
 
+	// Load default config
+	defaultConf, err := bindata.Asset("app.conf")
+	if err != nil {
+		err = fmt.Errorf("Cannot load asset app.conf: %s", err.Error())
+		return cfg, err
+	}
+	err = json.Unmarshal(defaultConf, &cfg)
+	if err != nil {
+		err = fmt.Errorf("Cannot unmarshall defaultConf: %s", err.Error())
+		return appConf{}, err
+	}
+
 	// Create conf if not exists
 	if _, err := os.Stat(cfgPath); err != nil {
-		conf, err := bindata.Asset("app.conf")
-		if err != nil {
-			err = fmt.Errorf("Cannot load asset app.conf: %s", err.Error())
-			return cfg, err
-		}
-		err = ioutil.WriteFile(cfgPath, conf, 0644)
+		err = ioutil.WriteFile(cfgPath, defaultConf, 0644)
 		if err != nil {
 			err = fmt.Errorf("Cannot write file %s: %s", strings.TrimLeft(cfgPath, pathu.Current), err.Error())
 			return cfg, err
@@ -104,11 +111,22 @@ func getAppCfg() (appConf, error) {
 		err = fmt.Errorf("Cannot read %s: %s", strings.TrimLeft(cfgPath, pathu.Current), err.Error())
 		return cfg, err
 	}
-
 	err = json.Unmarshal(raw, &cfg)
 	if err != nil {
 		err = fmt.Errorf("Cannot unmarshall %s: %s", strings.TrimLeft(cfgPath, pathu.Current), err.Error())
 		return appConf{}, err
+	}
+
+	// Write config
+	cfgJson, _ := json.MarshalIndent(cfg, "", "  ")
+	if err != nil {
+		err = fmt.Errorf("Cannot marshal config: %s", err.Error())
+		return appConf{}, err
+	}
+	err = ioutil.WriteFile(cfgPath, cfgJson, 0644)
+	if err != nil {
+		err = fmt.Errorf("Cannot write file %s: %s", strings.TrimLeft(cfgPath, pathu.Current), err.Error())
+		return cfg, err
 	}
 
 	return cfg, nil
