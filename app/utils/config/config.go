690287f5052f46c7e3e14cb6d5c97bad730ee65d
CrazyMax <crazy-max@users.noreply.github.com>
2017-10-17 02:10:49 +0200

    Bump version

--- app/utils/config/config.go
+++ app/utils/config/config.go
@@ -17,7 +17,7 @@ import (
 // App constants
 const (
 	NAME    = "WindowsSpyBlocker"
-	VERSION = "4.8.3"
+	VERSION = "4.9.0"
 	PACKAGE = "github.com/crazy-max/WindowsSpyBlocker"
 	URL     = "https://" + PACKAGE
 )
