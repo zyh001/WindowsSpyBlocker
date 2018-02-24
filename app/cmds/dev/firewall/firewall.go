5ff7d45575f8fcd16e1f7acce7b776881554bbbc
CrazyMax <crazy-max@users.noreply.github.com>
2018-02-24 03:31:46 +0100

    Manage ip range for firewall testing

--- app/cmds/dev/firewall/firewall.go
+++ app/cmds/dev/firewall/firewall.go
@@ -16,6 +16,7 @@ import (
 	"github.com/crazy-max/WindowsSpyBlocker/app/utils/timeu"
 	"github.com/crazy-max/WindowsSpyBlocker/app/whois"
 	"github.com/fatih/color"
+	"net"
 )
 
 // Menu of Firewall
@@ -81,44 +82,65 @@ func testIpsByRule(system string, rule string) {
 	testCsvFile, _ := os.Create(testCsv)
 	testCsvFile.WriteString("IP,ORGANIZATION,COUNTRY,RESOLVED DATE,RESOLVED DOMAIN")
 	for _, fwIp := range fwIps {
-		//TODO: Manage ip range for testing
 		if strings.Contains(fwIp.IP, "-") {
-			continue
+			testIpRange(fwIp.IP, testCsvFile)
 		} else if netu.IsValidIPv4(fwIp.IP) {
-			fmt.Print("\nTesting ")
-			color.New(color.FgMagenta).Printf("%s", fwIp.IP)
-			fmt.Print("...\n")
-			whoisResult := whois.GetWhois(fwIp.IP)
-			if whoisResult != (whois.Whois{}) {
-				fmt.Print("  Organisation: ")
-				color.New(color.FgCyan).Printf("%s\n", whoisResult.Org)
-				fmt.Print("  Country: ")
-				color.New(color.FgCyan).Printf("%s\n", whoisResult.Country)
-				testCsvFile.WriteString(fmt.Sprintf("\n%s,%s,%s", fwIp.IP, whoisResult.Org, whoisResult.Country))
-			} else {
-				continue
-			}
-			dnsresList := dnsres.GetDnsRes(fwIp.IP)
-			if len(dnsresList) > 0 {
-				countRes := 0
-				fmt.Println("  Resolutions:")
-				for _, res := range dnsresList {
-					fmt.Printf("    %s - ", res.LastResolved.Format("2006-01-02"))
-					color.New(color.FgCyan).Printf("%s\n", res.IpOrDomain)
-					if countRes == 0 {
-						testCsvFile.WriteString(fmt.Sprintf(",%s,%s", res.LastResolved.Format("2006-01-02"), res.IpOrDomain))
-					} else {
-						testCsvFile.WriteString(fmt.Sprintf("\n,,,%s,%s", res.LastResolved.Format("2006-01-02"), res.IpOrDomain))
-					}
-					countRes += 1
-				}
-			} else {
-				testCsvFile.WriteString(",,")
-			}
+			testIp(fwIp.IP, testCsvFile)
 		}
 	}
 
 	testCsvFile.Sync()
 	testCsvFile.Close()
 	fmt.Println()
+}
+
+func testIpRange(ipRange string, testCsvFile *os.File) {
+	ips, err := netu.GetIpsFromIPRange(ipRange)
+	if err != nil {
+		return
+	}
+	for _, ip := range ips {
+		ipNet := net.ParseIP(ip)
+		ipNet = ipNet.To4()
+		if ipNet == nil {
+			continue
+		}
+		//if ipNet[3] % 10 == 0 && ipNet[3] > 0 && ipNet[3] < 255 {
+		if ipNet[3] > 0 && ipNet[3] < 255 {
+			testIp(ip, testCsvFile)
+		}
+	}
+}
+
+func testIp(ip string, testCsvFile *os.File) {
+	fmt.Print("\nTesting ")
+	color.New(color.FgMagenta).Printf("%s", ip)
+	fmt.Print("...\n")
+	whoisResult := whois.GetWhois(ip)
+	if whoisResult != (whois.Whois{}) {
+		fmt.Print("  Organisation: ")
+		color.New(color.FgCyan).Printf("%s\n", whoisResult.Org)
+		fmt.Print("  Country: ")
+		color.New(color.FgCyan).Printf("%s\n", whoisResult.Country)
+		testCsvFile.WriteString(fmt.Sprintf("\n%s,%s,%s", ip, whoisResult.Org, whoisResult.Country))
+	} else {
+		return
+	}
+	dnsresList := dnsres.GetDnsRes(ip)
+	if len(dnsresList) > 0 {
+		countRes := 0
+		fmt.Println("  Resolutions:")
+		for _, res := range dnsresList {
+			fmt.Printf("    %s - ", res.LastResolved.Format("2006-01-02"))
+			color.New(color.FgCyan).Printf("%s\n", res.IpOrDomain)
+			if countRes == 0 {
+				testCsvFile.WriteString(fmt.Sprintf(",%s,%s", res.LastResolved.Format("2006-01-02"), res.IpOrDomain))
+			} else {
+				testCsvFile.WriteString(fmt.Sprintf("\n,,,%s,%s", res.LastResolved.Format("2006-01-02"), res.IpOrDomain))
+			}
+			countRes += 1
+		}
+	} else {
+		testCsvFile.WriteString(",,")
+	}
 }
