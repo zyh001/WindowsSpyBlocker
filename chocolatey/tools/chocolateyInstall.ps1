efdb3409929059aba77a0099424a7855e67512ba
CrazyMax <crazy-max@users.noreply.github.com>
2019-08-24 20:11:15 +0200

    Add Chocolatey package (Issue #143)

---
+++ chocolatey/tools/chocolateyInstall.ps1
@@ -0,0 +1,6 @@
+﻿$toolsDir = Split-Path $MyInvocation.MyCommand.Definition
+$desktopPath = [Environment]::GetFolderPath("Desktop")
+$lnkPath = $desktopPath + "\WindowsSpyBlocker.lnk"
+$exePath = Join-Path $toolsDir "WindowsSpyBlocker.exe"
+
+Install-ChocolateyShortcut -shortcutFilePath $lnkPath -targetPath $exePath
